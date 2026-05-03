a0df45ddfdeb734da7305e1235d0daed51f9aeb2
Achref Othmani <othmeniachref@gmail.com>
2026-05-03 11:20:38 +0100

    feat: add MapPicker component using Leaflet

---
+++ components/ui/MapPicker.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix for default marker icon in Leaflet + Next.js
+const icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+interface MapPickerProps {
+  latitude?: number | null;
+  longitude?: number | null;
+  onChange: (lat: number, lng: number) => void;
+}
+
+function MapEvents({ onChange }: { onChange: (lat: number, lng: number) => void }) {
+  useMapEvents({
+    click(e) {
+      onChange(e.latlng.lat, e.latlng.lng);
+    },
+  });
+  return null;
+}
+
+function ChangeView({ center }: { center: [number, number] }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(center);
+  }, [center, map]);
+  return null;
+}
+
+export function MapPicker({ latitude, longitude, onChange }: MapPickerProps) {
+  const defaultCenter: [number, number] = [36.8065, 10.1815]; // Tunis
+  const center: [number, number] = latitude && longitude ? [latitude, longitude] : defaultCenter;
+
+  return (
+    <div className="h-[300px] w-full rounded-[12px] overflow-hidden border border-line relative z-0">
+      <MapContainer
+        center={center}
+        zoom={13}
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MapEvents onChange={onChange} />
+        {latitude && longitude && (
+          <>
+            <ChangeView center={[latitude, longitude]} />
+            <Marker 
+              position={[latitude, longitude]} 
+              icon={icon}
+              draggable={true}
+              eventHandlers={{
+                dragend: (e) => {
+                  const marker = e.target;
+                  const position = marker.getLatLng();
+                  onChange(position.lat, position.lng);
+                },
+              }}
+            />
+          </>
+        )}
+      </MapContainer>
+    </div>
+  );
+}
